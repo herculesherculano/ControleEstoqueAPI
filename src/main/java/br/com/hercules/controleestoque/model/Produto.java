9412fd131749c9700dcc02d2e97e34e5d9e31fcb
Hercules Herculano <hbheroi@gmail.com>
2025-01-29 14:53:40 -0300

    implementacao das classes de modelo

---
+++ src/main/java/br/com/hercules/controleestoque/model/Produto.java
@@ -0,0 +1,50 @@
+package br.com.hercules.controleestoque.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    private int quantidade;
+
+    private double preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
